8d8fd4d2093c50666cc8f146e766d7aaf5040c7e
Szász Benedek <benedekszasz7@gmail.com>
2026-05-06 14:08:36 +0200

    kezdés

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 8000
+
+app.get('/', (req, res) => {
+  res.send('Helló Express!')
+})
+
+app.listen(port, () => {
+  console.log(`Figyelek a következő porton: ${port}`)
+})
+
